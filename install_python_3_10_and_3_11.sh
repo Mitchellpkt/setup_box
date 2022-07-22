e30889685ddd6362fa4485c069d4d7cb843bfcf2
Mitchell P. Krawiec-Thayer <mitchellpkt+github@protonmail.com>
2022-07-22 16:25:08 -0600

    chore: misc env setup

---
+++ install_python_3_10_and_3_11.sh
@@ -0,0 +1,20 @@
+# Instructions from https://www.linuxcapable.com/how-to-install-python-3-11-on-ubuntu-22-04-lts/
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+
+sudo apt install python3.10 -y
+sudo apt install python3.10-dev -y
+sudo apt install python3.10-venv -y
+sudo apt install python3.10-distutils -y
+sudo apt install python3.10-lib2to3 -y
+sudo apt install python3.10-gdbm -y
+sudo apt install python3.10-tk -y
+
+sudo apt install python3.11 -y
+sudo apt install python3.11-dev -y
+sudo apt install python3.11-venv -y
+sudo apt install python3.11-distutils -y
+sudo apt install python3.11-lib2to3 -y
+sudo apt install python3.11-gdbm -y
+sudo apt install python3.11-tk -y
