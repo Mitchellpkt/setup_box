b2539e62089ed11a795b0b682746290fba59d1c3
Mitchell P. Krawiec-Thayer <mitchellpkt+github@protonmail.com>
2022-08-22 14:19:39 -0600

    tweak: add terminal corrections

--- install_shell_stuff.sh
+++ install_shell_stuff.sh
@@ -1,9 +1,18 @@
 # install zsh and use oh-my-zsh
 sudo apt install zsh
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh; zsh
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# install thefuck
+sudo apt update
+sudo apt install python3-dev python3-pip python3-setuptools
+pip3 install thefuck --user
+
+# alias
+echo "eval $(thefuck --alias fuck)">>~/.zshrc
+echo "eval $(thefuck --alias oops)">>~/.zshrc
 
 # change the shell for the future
 chsh -s $(which zsh)
 
-# change the shell now
+# fresh shell for now
 zsh
